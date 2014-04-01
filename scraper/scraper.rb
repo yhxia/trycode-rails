9ec0f263617aeedb6884e2425b6e68f03f8f4ea7
yhxia <yinghuix@qq.com>
2014-04-01 14:27:09 +0800

    try to use global name

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -6,11 +6,11 @@ scraper = YunyunScraper.new
 if ARGV[0]=="-addp"
   # read proxy.list
   # [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}.*[0-9]{1,5}
-  File.readlines("proxy.list").each do |each_proxy|
-    each_proxy = each_proxy.strip
-    puts "Specting "+each_proxy+" ..."
-    if !Proxy.exists?(:addr=> each_proxy)
-      Proxy.create({:addr=>each_proxy,:speed=>3600})
+  File.readlines("proxy.list").each do |proxy|
+    proxy = proxy.strip
+    puts "Specting "+proxy+" ..."
+    if !Proxy.exists?(:addr=> proxy)
+      Proxy.create({:addr=>proxy,:speed=>3600})
       puts "\e[32mSaved!\e[0m"
     else
       puts "\e[31mExisted!\e[0m"
@@ -22,36 +22,40 @@ end
 # test proxy
 if ARGV[0]=="-testp"
   while true do
-    proxy_list = Proxy.where("speed = 3600")
-    break if proxy_list.nil?
-    r = Random.new(Time.now.to_i+Process.pid)
-    n = r.rand(0..(proxy_list.length-2))
-    proxy_list = proxy_list.limit(1).offset(n)
-    if proxy_list.first.using == 1
+    proxy_list = Proxy.where("speed = 3600 AND `using`=0")
+    if proxy_list.length == 0
+      puts "Nothing."
+      break 
+    end
+    # proxy_list = proxy_list.first
+    # r = Random.new(Time.now.to_i+Process.pid)
+    # n = r.rand(0..(proxy_list.length-1))
+    # proxy_list = proxy_list.limit(1).offset(n)
+    proxy = proxy_list.first
+
+    if proxy.using == 1
       next
     else
-      proxy_list.first.using = 1
+      proxy.using = 1
     end
-    proxy_list.first.save!
+    proxy.save!
 
-    proxy_list.each do |each_proxy|
-      puts each_proxy.addr
-      start_time = Time.now
-      puts "\e[32mStart Time: %10.4f" % start_time.to_f+"\e[0m"
+    puts proxy.addr
+    start_time = Time.now
+    puts "\e[32mStart Time: %10.4f" % start_time.to_f+"\e[0m"
 
-      if scraper.fetchContentByPhantomjs2("",each_proxy.addr).length > 199
-        stop_time = Time.now
-        puts "\e[32mStop Time: %10.4f" % stop_time.to_f+"\e[0m"
-        cost_time = (stop_time - start_time)
-        puts "\e[32mCost Time: %.4f" % cost_time.to_f + " s"+"\e[0m"
-        each_proxy.speed = cost_time.to_i
-      else
-        puts "\e[31mWrong! Speed updated to 7200..."+"\e[0m"
-        each_proxy.speed = 7200
-      end
-      proxy_list.first.using = 0
-      each_proxy.save!
+    if scraper.fetchContentByPhantomjs2("",proxy.addr).length > 199
+      stop_time = Time.now
+      puts "\e[32mStop Time: %10.4f" % stop_time.to_f+"\e[0m"
+      cost_time = (stop_time - start_time)
+      puts "\e[32mCost Time: %.4f" % cost_time.to_f + " s"+"\e[0m"
+      proxy.speed = cost_time.to_i
+    else
+      puts "\e[31mWrong! Speed updated to 7200..."+"\e[0m"
+      proxy.speed = 7200
     end
+    proxy.using = 0
+    proxy.save!
   end
   exit
 end
@@ -107,7 +111,9 @@ if ARGV[0]=="-fetch"
       puts "\e[32m[Fetching] '"+each_url[1]+"' "+each_url[0]+" ...\e[0m"
       url = each_url[0]
       project = each_url[1]
-      sleep(Random.new(125).rand(10))
+      #sleep(Random.new(125).rand(10))
+      #Todo: add download part and get proxy from db
+
       thr_num -= 1
       puts "\e[32m[Done thread"+index.to_s+"] "+thr_num.to_s+" threads left.\e[0m"
     }
